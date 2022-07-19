aa571f6eab8c80ea2b041147dfb29f96ca6064d2
Nursultan24 <Nursultanmustafaev15@gmail.com>
2022-07-19 19:01:16 +0600

    'initial'

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+//кнопка для обратной связи
+
+let callBtn = document.querySelector('.btn_call');
+let overlay = document.querySelector('.overlay');
+let close = document.querySelector('.popup__close');
+let call = document.querySelector('.popup__call');
+let test = document.querySelector('.popup__test');
+let rent = document.querySelector('.popup__rent')
+
+let btnTest = document.querySelector('.btn_test');
+
+let btnRent = document.querySelector('.btn_rent');
+
+callBtn.addEventListener('click', function () {
+   overlay.style.display = 'block';
+   test.style.display = 'none';
+   rent.style.display= 'none';
+   call.style.display = 'flex'
+
+
+});
+
+btnTest.addEventListener('click', function () {
+   overlay.style.display = 'block';
+   call.style.display = 'none';
+   rent.style.display= 'none';
+   test.style.display = 'flex'
+
+
+
+});
+
+btnRent.addEventListener('click', function () {
+   overlay.style.display = 'block';
+   call.style.display = 'none';
+   test.style.display = 'none';
+   rent.style.display= 'flex';
+});
+
+close.addEventListener('click', function () {
+   overlay.style.display = 'none'
+});
+
+overlay.addEventListener('click', function (e) {
+
+   if (e.target.className.includes('overlay')){
+      overlay.style.display = 'none'
+   }
+
+});
+
+
